4f06cd93210f7819a44b80e6737422ba2df3c74f
structure7 <kemperm@usa.net>
2016-07-18 21:28:36 -0700

    OTA

--- hvacMonitorNode04.ino
+++ hvacMonitorNode04.ino
@@ -2,16 +2,18 @@
 #define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
+
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #define ONE_WIRE_BUS 2
-
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
-float tempMK; // Room temp
-//int tempMKhighAlarm = 200;
+#include <ESP8266mDNS.h>        // Required for OTA
+#include <WiFiUdp.h>            // Required for OTA
+#include <ArduinoOTA.h>         // Required for OTA
 
+float tempMK; // Room temp
 char auth[] = "fromBlynkApp";
 
 SimpleTimer timer;
@@ -24,13 +26,52 @@ void setup()
   Serial.begin(9600);
   Blynk.begin(auth, "ssid", "pw");
 
+  while (Blynk.connect() == false) {
+    // Wait until connected
+  }
+
   WiFi.softAPdisconnect(true); // Per https://github.com/esp8266/Arduino/issues/676 this turns off AP
 
   sensors.begin();
   sensors.setResolution(10);
 
+  // START OTA ROUTINE
+  ArduinoOTA.setHostname("esp8266-HVAC");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("MAC address: ");
+  Serial.println(WiFi.macAddress());
+  // END OTA ROUTINE
+
   timer.setInterval(2000L, sendTemps); // Temperature sensor polling interval
   heartbeatOn();
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+  ArduinoOTA.handle();
 }
 
 BLYNK_WRITE(V27) // App button to report uptime
@@ -39,16 +80,24 @@ BLYNK_WRITE(V27) // App button to report uptime
 
   if (pinData == 0)
   {
-    timer.setTimeout(8000L, uptimeSend);
+    timer.setTimeout(12000L, uptimeSend);
   }
 }
 
-void uptimeSend()  // Blinks a virtual LED in the Blynk app to show the ESP is live and reporting.
+void uptimeSend()
 {
-  float secDur = millis() / 1000;
-  float minDur = secDur / 60;
-  float hourDur = minDur / 60;
-  terminal.println(String("Node04 (MK): ") + hourDur + " hours ");
+  long minDur = millis() / 60000L;
+  long hourDur = millis() / 3600000L;
+  if (minDur < 121)
+  {
+    terminal.print(String("Node04 (MK): ") + minDur + " mins @ ");
+    terminal.println(WiFi.localIP());
+  }
+  else if (minDur > 120)
+  {
+    terminal.print(String("Node04 (MK): ") + hourDur + " hrs @ ");
+    terminal.println(WiFi.localIP());
+  }
   terminal.flush();
 }
 
@@ -78,60 +127,4 @@ void sendTemps()
   {
     Blynk.virtualWrite(31, "ERR");
   }
-
-  /*
-    if (tempMK >= tempMKhighAlarm)
-    {
-      notifyAndOff();
-    }
-    */
 }
-
-void loop()
-{
-  Blynk.run();
-  timer.run();
-}
-
-/*
-// Input from Blynk app menu to select room temperature that triggers alarm
-BLYNK_WRITE(V21) {
-  switch (param.asInt())
-  {
-    case 1: { // Alarm Off
-        tempMKhighAlarm = 200;
-        break;
-      }
-    case 2: { // 80F Alarm
-        tempMKhighAlarm = 80;
-        break;
-      }
-    case 3: { // 81F Alarm
-        tempMKhighAlarm = 81;
-        break;
-      }
-    case 4: { // 82F Alarm;
-        tempMKhighAlarm = 82;
-        break;
-      }
-    case 5: { // 83F Alarm
-        tempMKhighAlarm = 83;
-        break;
-      }
-    case 6: { // 84F Alarm
-        tempMKhighAlarm = 84;
-        break;
-      }
-    default: {
-        Serial.println("Unknown item selected");
-      }
-  }
-}
-
-void notifyAndOff()
-{
-  Blynk.notify(String("My room is ") + tempMK + "°F. Alarm disabled until reset."); // Send notification.
-  Blynk.virtualWrite(V21, 1); // Rather than fancy timing, just disable alarm until reset.
-  tempMKhighAlarm = 200;
-}
-*/
